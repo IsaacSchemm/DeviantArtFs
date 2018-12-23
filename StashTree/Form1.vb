f2547e0ce8e0bc105f3e66d20bfcea2e5241910c
libertyernie <maybeway36@gmail.com>
2018-12-23 17:54:25 -0600

    Hide positional placeholders in StashTree app

--- StashTree/Form1.vb
+++ StashTree/Form1.vb
@@ -47,6 +47,10 @@ Public Class Form1
 
     Private Sub AddNodes(node As TreeNode, nodes As IEnumerable(Of StashNode))
         For Each n In nodes
+            If TypeOf n Is StashPlaceholderNode Then
+                Continue For
+            End If
+
             Dim stackNode = node.Nodes.Add($"{n.Title} ({n.GetType().Name})")
             NodeToItem.Add(stackNode, n)
             If TypeOf n Is StashStack Then
