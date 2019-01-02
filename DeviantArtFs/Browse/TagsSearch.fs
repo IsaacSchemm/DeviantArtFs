d63187b5ca1ee1dcb0d23cc0e5531ed83429a4ba
libertyernie <maybeway36@gmail.com>
2019-01-02 12:44:25 -0600

    Add Browse.Tags and Browse.TagsSearch

---
+++ DeviantArtFs/Browse/TagsSearch.fs
@@ -0,0 +1,30 @@
+﻿namespace DeviantArtFs.Browse
+
+open DeviantArtFs
+open FSharp.Data
+
+type TagsResponse = JsonProvider<"""{
+    "results": [
+        {
+            "tag_name": "animal"
+        },
+        {
+            "tag_name": "animation"
+        },
+        {
+            "tag_name": "anime"
+        }
+    ]
+}""">
+
+module TagsSearch =
+    let AsyncExecute token (tag_name: string) = async {
+        let req =
+            sprintf "https://www.deviantart.com/api/v1/oauth2/browse/tags/search?tag_name=%s" (dafs.urlEncode tag_name)
+            |> dafs.createRequest token
+        let! json = dafs.asyncRead req
+        let o = TagsResponse.Parse json
+        return o.Results |> Seq.map (fun t -> t.TagName)
+    }
+
+    let ExecuteAsync token tag_name = AsyncExecute token tag_name |> Async.StartAsTask
