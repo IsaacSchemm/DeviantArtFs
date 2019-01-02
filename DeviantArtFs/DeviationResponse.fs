96e471542c0969af0a1b9e4fb90e276e51a8ef19
libertyernie <maybeway36@gmail.com>
2019-01-02 13:03:21 -0600

    Add more samples to DeviationResponse

--- DeviantArtFs/DeviationResponse.fs
+++ DeviantArtFs/DeviationResponse.fs
@@ -97,7 +97,221 @@ type DeviationResponse = JsonProvider<"""[{
     },
     "is_mature": false,
     "is_downloadable": false
-}, {
+},
+{
+    "deviationid": "DDC95228-CE96-B883-988D-B20FDA4A751F",
+    "printid": null,
+    "url": "https://www.deviantart.com/chi-u/art/VIDEO-Angled-face-tutorial-330889956",
+    "title": "[VIDEO] - Angled face tutorial",
+    "category": "Drawing & Inking",
+    "category_path": "resources/filmresources/techtutorials/digital/drawink",
+    "is_favourited": false,
+    "is_deleted": false,
+    "author": {
+        "userid": "394C3FC5-D6E5-FD5D-FAD0-BAFF08A00A61",
+        "username": "chi-u",
+        "usericon": "https://a.deviantart.net/avatars/c/h/chi-u.gif?11",
+        "type": "regular"
+    },
+    "stats": {
+        "comments": 101,
+        "favourites": 1918
+    },
+    "published_time": "1349487153",
+    "allows_comments": true,
+    "preview": {
+        "src": "https://other00.deviantart.net/8869/o/2012/279/f/7/f7f47f36b2b9c263d68d751afa847e0d.jpg",
+        "height": 1014,
+        "width": 1920,
+        "transparency": false
+    },
+    "thumbs": [
+        {
+            "src": "https://pre00.deviantart.net/5371/th/film/o/2012/279/f/7/f7f47f36b2b9c263d68d751afa847e0d.jpg",
+            "height": 79,
+            "width": 150,
+            "transparency": false
+        },
+        {
+            "src": "https://pre00.deviantart.net/7ef6/th/film200h/o/2012/279/f/7/f7f47f36b2b9c263d68d751afa847e0d.jpg",
+            "height": 158,
+            "width": 300,
+            "transparency": false
+        }
+    ],
+    "videos": [
+        {
+            "src": "https://film00.deviantart.net/a9d5/v/mp4/2012/279/e/a/_video____angled_face_tutorial_by_chi_u-d5h04bo.mp4",
+            "quality": "360p",
+            "filesize": 5452441,
+            "duration": 289
+        },
+        {
+            "src": "https://film00.deviantart.net/fa73/v/mp4/2012/279/8/8/_video____angled_face_tutorial_by_chi_u-d5h04bo.mp4",
+            "quality": "720p",
+            "filesize": 11584847,
+            "duration": 289
+        },
+        {
+            "src": "https://film00.deviantart.net/4685/v/mp4/2012/279/2/1/_video____angled_face_tutorial_by_chi_u-d5h04bo.mp4",
+            "quality": "1080p",
+            "filesize": 16782531,
+            "duration": 289
+        }
+    ],
+    "is_mature": false,
+    "is_downloadable": false
+},
+{
+    "deviationid": "2FB81F74-389E-CB75-9F3F-7387F4DBDA7A",
+    "printid": null,
+    "url": "https://www.deviantart.com/ecokitty/art/Mouse-Flash-193246799",
+    "title": "Mouse -Flash-",
+    "category": "Animations",
+    "category_path": "flash/animations",
+    "is_favourited": false,
+    "is_deleted": false,
+    "author": {
+        "userid": "D8481837-34DA-7773-39CE-97B23E008CA9",
+        "username": "ecokitty",
+        "usericon": "https://a.deviantart.net/avatars/e/c/ecokitty.gif?5",
+        "type": "regular"
+    },
+    "stats": {
+        "comments": 283,
+        "favourites": 1018
+    },
+    "published_time": "1294887513",
+    "allows_comments": true,
+    "preview": {
+        "src": "https://img00.deviantart.net/a3b7/i/2011/012/8/8/mouse__flash__by_ecokitty-d371y6n.gif",
+        "height": 150,
+        "width": 150,
+        "transparency": false
+    },
+    "content": {
+        "src": "https://img00.deviantart.net/a3b7/i/2011/012/8/8/mouse__flash__by_ecokitty-d371y6n.gif",
+        "height": 150,
+        "width": 150,
+        "transparency": false,
+        "filesize": 26742
+    },
+    "thumbs": [
+        {
+            "src": "https://img00.deviantart.net/a3b7/i/2011/012/8/8/mouse__flash__by_ecokitty-d371y6n.gif",
+            "height": 150,
+            "width": 150,
+            "transparency": false
+        },
+        {
+            "src": "https://img00.deviantart.net/a3b7/i/2011/012/8/8/mouse__flash__by_ecokitty-d371y6n.gif",
+            "height": 150,
+            "width": 150,
+            "transparency": false
+        },
+        {
+            "src": "https://img00.deviantart.net/a3b7/i/2011/012/8/8/mouse__flash__by_ecokitty-d371y6n.gif",
+            "height": 150,
+            "width": 150,
+            "transparency": false
+        }
+    ],
+    "flash": {
+        "src": "https://orig00.deviantart.net/9434/f/2011/012/4/a/mouse__flash__by_ecokitty-d371y6n.swf",
+        "height": 400,
+        "width": 700
+    },
+    "is_mature": false,
+    "is_downloadable": true,
+    "download_filesize": 260557
+},
+{
+    "deviationid": "4E3766C3-515E-CF16-1341-D42E46B01FC1",
+    "printid": null,
+    "url": "https://www.deviantart.com/lizard-socks/journal/CrosspostSharp-3-5-776866366",
+    "title": "CrosspostSharp 3.5",
+    "category": "Personal",
+    "category_path": "journals/personal",
+    "is_favourited": false,
+    "is_deleted": false,
+    "author": {
+        "userid": "7EE6490E-FCA7-3129-410A-AA684C3BC7DB",
+        "username": "lizard-socks",
+        "usericon": "https://a.deviantart.net/avatars/l/i/lizard-socks.png",
+        "type": "regular"
+    },
+    "stats": {
+        "comments": 0,
+        "favourites": 0
+    },
+    "published_time": "1544925280",
+    "allows_comments": true,
+    "thumbs": [],
+    "excerpt": "I posted a new release of CrosspostSharp again! It's a Windows app for cross-posting your own art posts from one site to another (allowing you to adjust the description text and other settings on a per-site basis.)<br /><br />This version adds Mastodon support and makes a couple things nicer.<br /><br />https://github.com/libertyernie/CrosspostSharp/releases/tag/v3.5<br /><br />In case you're curious, it's a WinForms app written in a mix of C# and F#, and it has some libraries you could reuse if you want to write your own .NET stuff.",
+    "is_mature": false,
+    "is_downloadable": false
+},
+{
+    "deviationid": "CC79FBC8-735B-CF92-88AF-D1FCAE147939",
+    "printid": null,
+    "url": "https://www.deviantart.com/arkeis-pokemon/art/BFOI-Round-4-440614315",
+    "title": "BFOI Round 4",
+    "category": "Scraps",
+    "category_path": "scraps",
+    "is_favourited": false,
+    "is_deleted": false,
+    "author": {
+        "userid": "1411C535-28EE-959E-8399-762C68E41B8F",
+        "username": "arkeis-pokemon",
+        "usericon": "https://a.deviantart.net/avatars/a/r/arkeis-pokemon.gif?1",
+        "type": "regular"
+    },
+    "stats": {
+        "comments": 10,
+        "favourites": 35
+    },
+    "published_time": "1394926088",
+    "allows_comments": true,
+    "preview": {
+        "src": "https://orig00.deviantart.net/d7c7/f/2014/074/9/0/bfoi_r4_by_arkeis_pokemon-d7abw6j.jpg",
+        "height": 200,
+        "width": 150,
+        "transparency": false
+    },
+    "content": {
+        "src": "https://orig00.deviantart.net/d7c7/f/2014/074/9/0/bfoi_r4_by_arkeis_pokemon-d7abw6j.jpg",
+        "height": 200,
+        "width": 150,
+        "transparency": false,
+        "filesize": 31461
+    },
+    "thumbs": [
+        {
+            "src": "https://t00.deviantart.net/laHmUQmSb2j7on09SrCaoHirCfE=/fit-in/150x150/filters:no_upscale():origin()/pre00/c499/th/pre/f/2014/074/9/0/bfoi_r4_by_arkeis_pokemon-d7abw6j.jpg",
+            "height": 150,
+            "width": 113,
+            "transparency": false
+        },
+        {
+            "src": "https://orig00.deviantart.net/d7c7/f/2014/074/9/0/bfoi_r4_by_arkeis_pokemon-d7abw6j.jpg",
+            "height": 200,
+            "width": 150,
+            "transparency": false
+        },
+        {
+            "src": "https://orig00.deviantart.net/d7c7/f/2014/074/9/0/bfoi_r4_by_arkeis_pokemon-d7abw6j.jpg",
+            "height": 200,
+            "width": 150,
+            "transparency": false
+        }
+    ],
+    "is_mature": false,
+    "is_downloadable": false,
+    "motion_book": {
+        "embed_url": "https://backend.deviantart.com/embed/madefire/440614315"
+    }
+},
+{
     "deviationid": "00000000-0000-0000-0000-000000000000",
     "printid": "00000000-0000-0000-0000-000000000000",
     "is_deleted": true
