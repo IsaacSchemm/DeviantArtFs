39d83ae65eb2b4a4e744dbc1ef2ca35433667a35
libertyernie <maybeway36@gmail.com>
2019-01-06 20:29:14 -0600

    Add a small C# program to generate a list of functions in DeviantArtFs

---
+++ DocGenerator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace DocGenerator
+{
+    class Program
+    {
+        static string PrintTypeName(Type t)
+        {
+            string n = t.Name.Replace("`1", "").Replace("`2", "").Replace("IJsonDocument", "???");
+            if (n == "FSharpAsync") n = "Async";
+            if (n == "Boolean") n = "bool";
+            if (n == "Int32") n = "int";
+            return $"{n}{(t.GenericTypeArguments.Any() ? $"<{string.Join(",", t.GenericTypeArguments.Select(x => PrintTypeName(x)))}>" : "")}";
+        }
+
+        static void Main(string[] args)
+        {
+            var a = Assembly.GetAssembly(typeof(DeviantArtFs.Requests.Browse.CategoryTree));
+            foreach (var t in a.GetTypes())
+            {
+                if (t.Name.Contains("@")) continue;
+                var ae = t.GetMembers().Select(x => x as MethodInfo).Where(x => x != null).Where(x => x.ReturnType.Name.StartsWith("Task") || x.ReturnType.Name.StartsWith("FSharpAsync"));
+                if (ae.Any())
+                {
+                    Console.WriteLine(t.FullName);
+                    foreach (var x in ae)
+                    {
+                        Console.Write("* " + x.Name);
+                        foreach (var p in x.GetParameters())
+                        {
+                            Console.Write($" ({PrintTypeName(p.ParameterType)})");
+                        }
+                        Console.WriteLine(" -> " + PrintTypeName(x.ReturnType));
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
