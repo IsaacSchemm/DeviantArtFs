5961ce6dd262c47d3a108a737234a50b7aca1991
libertyernie <maybeway36@gmail.com>
2019-01-06 21:21:26 -0600

    Add endpoints.txt

--- DocGenerator/Program.cs
+++ DocGenerator/Program.cs
@@ -12,27 +12,32 @@ namespace DocGenerator
             if (n == "FSharpAsync") n = "Async";
             if (n == "Boolean") n = "bool";
             if (n == "Int32") n = "int";
-            return $"{n}{(t.GenericTypeArguments.Any() ? $"<{string.Join(",", t.GenericTypeArguments.Select(x => PrintTypeName(x)))}>" : "")}";
+            if (n == "Int64") n = "long";
+            if (n == "String") n = "string";
+            return $"{n}{(t.GenericTypeArguments.Any() ? $"<{string.Join(", ", t.GenericTypeArguments.Select(x => PrintTypeName(x)))}>" : "")}";
         }
 
         static void Main(string[] args)
         {
             var a = Assembly.GetAssembly(typeof(DeviantArtFs.Requests.Browse.CategoryTree));
-            foreach (var t in a.GetTypes())
+            foreach (var t in a.GetTypes().OrderBy(x => x.FullName))
             {
                 if (t.Name.Contains("@")) continue;
-                var ae = t.GetMembers().Select(x => x as MethodInfo).Where(x => x != null).Where(x => x.ReturnType.Name.StartsWith("Task") || x.ReturnType.Name.StartsWith("FSharpAsync"));
+                var ae = t.GetMembers()
+                    .Select(x => x as MethodInfo)
+                    .Where(x => x != null)
+                    .Where(x => x.ReturnType.Name.StartsWith("Task") || x.ReturnType.Name.StartsWith("FSharpAsync"));
                 if (ae.Any())
                 {
                     Console.WriteLine(t.FullName);
                     foreach (var x in ae)
                     {
-                        Console.Write("* " + x.Name);
+                        Console.Write($"* {x.Name}");
                         foreach (var p in x.GetParameters())
                         {
                             Console.Write($" ({PrintTypeName(p.ParameterType)})");
                         }
-                        Console.WriteLine(" -> " + PrintTypeName(x.ReturnType));
+                        Console.WriteLine($" -> {PrintTypeName(x.ReturnType)}");
                     }
                     Console.WriteLine();
                 }
