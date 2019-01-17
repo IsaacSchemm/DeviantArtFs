244b8c47e4672adc06fefe04eefbd3acfd8b5f15
libertyernie <maybeway36@gmail.com>
2019-01-17 12:17:16 -0600

    Remove old VB.NET example

--- DeviantArtFs.Examples.StashTree/AccessToken.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class AccessToken
-    Implements IDeviantArtAccessToken
-
-    Public Sub New(token As String)
-        AccessToken = token
-    End Sub
-
-    Public ReadOnly Property AccessToken As String Implements IDeviantArtAccessToken.AccessToken
-End Class
