2f9ee86e2c095b860f50999e027874a854573f17
libertyernie <maybeway36@gmail.com>
2019-01-28 08:40:13 -0600

    Add feed filter

--- DeviantArtFs.Examples/WebApp/Models/FeedItemModel.cs
+++ DeviantArtFs.Examples/WebApp/Models/FeedItemModel.cs
@@ -15,6 +15,9 @@ namespace DeviantArtFs.Examples.WebApp.Models
             _item = item ?? throw new ArgumentNullException(nameof(item));
         }
 
+        public string Type => _item.Type;
+        public string Username => _item.ByUser.Username;
+        public string Usericon => _item.ByUser.Usericon;
         public string Url =>
             _item.Deviations.FirstOrDefault()?.Url
             ?? _item.Status?.Url
@@ -23,8 +26,6 @@ namespace DeviantArtFs.Examples.WebApp.Models
             _item.Deviations.FirstOrDefault()?.Thumbs?.FirstOrDefault()?.Src;
         public string Title =>
             _item.Deviations.FirstOrDefault()?.Title;
-        public string Username => _item.ByUser.Username;
-        public string Usericon => _item.ByUser.Usericon;
         public string HTMLDescription =>
             _item.Type == "collection_update" ? $"Updated collection <a href='{_item.Collection.Url}'>{WebUtility.HtmlEncode(_item.Collection.Name)}</a>"
             : _item.Type == "status" ? _item.Status?.Body
