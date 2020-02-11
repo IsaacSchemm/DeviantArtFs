184b4fb20aba01681e83a3e7f4ae6d317831aa05
Isaac Schemm <isaacschemm@gmail.com>
2020-02-10 21:16:28 -0600

    Remove unused module

--- DeviantArtFs/PagedResponses/DeviantArtPagedResult.fs
+++ DeviantArtFs/PagedResponses/DeviantArtPagedResult.fs
@@ -13,10 +13,3 @@ type DeviantArtPagedResult<'a> = {
         member this.HasMore = this.has_more
         member this.Cursor = this.next_offset |> Option.defaultValue 0
         member this.Items = this.results |> Seq.ofList
-
-module internal Page =
-    let map f (this: DeviantArtPagedResult<'a>) = {
-        has_more = this.has_more
-        next_offset = this.next_offset
-        results = List.map f this.results
-    }
