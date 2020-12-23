6f25c5f24b2f2378b8c98f8cf540d4c213ace38c
Isaac Schemm <isaacschemm@gmail.com>
2020-12-23 16:52:20 -0600

    Start adding common params to collection endpoints

--- DeviantArtFs/Api/Browse/TopTopics.fs
+++ DeviantArtFs/Api/Browse/TopTopics.fs
@@ -4,14 +4,19 @@ open DeviantArtFs
 open FSharp.Control
 
 module TopTopics =
-    let AsyncExecute token = async {
+    let AsyncExecute token common = async {
+        let query = seq {
+            yield! QueryFor.commonParams common
+        }
         let req =
-            sprintf "https://www.deviantart.com/api/v1/oauth2/browse/toptopics"
+            query
+            |> String.concat "&"
+            |> sprintf "https://www.deviantart.com/api/v1/oauth2/browse/toptopics?%s"
             |> Dafs.createRequest token
         let! json = Dafs.asyncRead req
         return DeviantArtListOnlyResponse<DeviantArtTopic>.ParseList json
     }
 
-    let ExecuteAsync token =
-        AsyncExecute token
+    let ExecuteAsync token common =
+        AsyncExecute token common
         |> Async.StartAsTask
--- DeviantArtFs/Api/Collections/CollectionById.fs
+++ DeviantArtFs/Api/Collections/CollectionById.fs
@@ -10,12 +10,13 @@ type CollectionByIdRequest(folderid: Guid) =
 module CollectionById =
     open FSharp.Control
 
-    let AsyncExecute token paging (req: CollectionByIdRequest) = async {
+    let AsyncExecute token common paging (req: CollectionByIdRequest) = async {
         let query = seq {
             match Option.ofObj req.Username with
             | Some s -> yield sprintf "username=%s" (Dafs.urlEncode s)
             | None -> ()
             yield! QueryFor.paging paging 24
+            yield! QueryFor.commonParams common
         }
         let req =
             query
@@ -26,16 +27,16 @@ module CollectionById =
         return DeviantArtFolderPagedResult.Parse json
     }
 
-    let ToAsyncSeq token offset req =
-        Dafs.getMax (AsyncExecute token)
-        |> Dafs.toAsyncSeq offset req
+    let ToAsyncSeq token common offset req =
+        (fun p -> AsyncExecute token common p req)
+        |> Dafs.toAsyncSeq2 offset
 
-    let ToArrayAsync token offset limit req =
-        ToAsyncSeq token offset req
+    let ToArrayAsync token common offset limit req =
+        ToAsyncSeq token common offset req
         |> AsyncSeq.take limit
         |> AsyncSeq.toArrayAsync
         |> Async.StartAsTask
 
-    let ExecuteAsync token paging req =
-        AsyncExecute token paging req
+    let ExecuteAsync token common paging req =
+        AsyncExecute token common paging req
         |> Async.StartAsTask
