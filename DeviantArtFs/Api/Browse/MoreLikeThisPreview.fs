097b0082631ce217837a6fdb031cbf250d05c0aa
Isaac Schemm <isaacschemm@gmail.com>
2020-12-25 22:01:46 -0600

    Add common param to MoreLikeThisPreview

--- DeviantArtFs/Api/Browse/MoreLikeThisPreview.fs
+++ DeviantArtFs/Api/Browse/MoreLikeThisPreview.fs
@@ -4,12 +4,15 @@ open System
 open DeviantArtFs
 
 module MoreLikeThisPreview =
-    let AsyncExecute token (seed: Guid) =
+    let AsyncExecute token common (seed: Guid) =
         seq {
             yield sprintf "seed=%O" seed
+            yield! QueryFor.commonParams common
         }
         |> Dafs.createRequest token "https://www.deviantart.com/api/v1/oauth2/browse/morelikethis/preview"
         |> Dafs.asyncRead
         |> Dafs.thenParse<DeviantArtMoreLikeThisPreviewResult>
 
-    let ExecuteAsync token seed = AsyncExecute token seed |> Async.StartAsTask
+    let ExecuteAsync token common seed =
+        AsyncExecute token common seed
+        |> Async.StartAsTask
