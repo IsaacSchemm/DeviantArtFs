4392abca95ee232f2ade6939b63d5e42d6be5c7e
Isaac Schemm <isaacschemm@gmail.com>
2021-01-08 11:43:38 -0600

    Add multi-targeting to allow IAsyncEnumerable support on .NET 5

--- DeviantArtFs/Extensions/Extensions.fs
+++ DeviantArtFs/Extensions/Extensions.fs
@@ -1,6 +1,7 @@
 ﻿namespace DeviantArtFs.Extensions
 
 open System.Runtime.CompilerServices
+open FSharp.Control
 
 [<Extension>]
 module OptionValueExtensions =
@@ -33,3 +34,11 @@ module OptionListExtensions =
     [<Extension>]
     let OrEmpty this =
         Option.defaultValue List.empty this
+
+#if NET5_0
+[<Extension>]
+module AsyncSeqExtensions =
+    [<Extension>]
+    let ToAsyncEnumerable this =
+        AsyncSeq.toAsyncEnum this
+#endif
---
+++ ExampleConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using DeviantArtFs;
+using DeviantArtFs.Extensions;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ExampleConsoleApp2 {
+    record Token : IDeviantArtAccessToken {
+        public string AccessToken { get; init; }
+    }
+
+    class Program {
+        static async Task Main() {
+            Console.Write("Please enter a DeviantArt access token: ");
+            var token = new Token { AccessToken = Console.ReadLine() };
+
+            var req = new DeviantArtFs.Api.Gallery.GalleryAllViewRequest();
+            int i = 0;
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            await foreach (var deviation in DeviantArtFs.Api.Gallery.GalleryAllView.ToAsyncSeq(token, req, 0).ToAsyncEnumerable()) {
+                Console.WriteLine($"[{st.Elapsed}] {i + 1}. {deviation.title}");
+                i++;
+                if (i > 100) break;
+            }
+            st.Stop();
+        }
+    }
+}
